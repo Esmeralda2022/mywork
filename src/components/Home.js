6c93d16402bfc9e441e72c294d1c79d63186c594
Esmeralda2022 <esmeraldafurriku93@gmail.com>
2022-04-25 00:41:22 +0200

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ export const Home = () => {
      
 return (
    
- <Container   className='containerhome' >
+ <Container fluid  className='containerhome' >
        <Row >
              <Col>
                 <span className ='text2'>{text}</span>
